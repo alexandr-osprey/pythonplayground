cf7348cddd544b43ffd51abb30b35491c866b0f7
Александр Козаченко <osprey.alexandr@gmail.com>
2020-05-16 13:06:40 +0300

    fixes

--- src/circle.py
+++ src/circle.py
@@ -6,9 +6,19 @@ def generate_data(size, test_part):
     radius = 5
     noise = 0
     positive_num = int(size / 2)
+    negative_num = size - positive_num
     positive_data = _generate_region(positive_num, radius, noise, 0, radius * 0.5)
-    negative_data = _generate_region(positive_num, radius, noise, radius, radius * 0.7)
-    return positive_data, negative_data
+    negative_data = _generate_region(negative_num, radius, noise, radius, radius * 0.7)
+    x = np.hstack((positive_data[0], negative_data[0])).reshape(1, size)
+    y = np.hstack((positive_data[1], negative_data[1])).reshape(1, size)
+    labels = np.hstack((positive_data[2], negative_data[2])).reshape(1, size)
+    all_data = np.vstack((x, y, labels))
+    all_data_t = all_data.T
+    np.random.shuffle(all_data_t)
+    shuffled_t = all_data_t.T
+    points = np.vstack((shuffled_t[0], shuffled_t[1]))
+    train, test = ds.split_data(points, shuffled_t[2].reshape(1, size), test_part)
+    return train, test
 
 
 def _generate_region(size, radius, noise, a, b):
@@ -19,8 +29,7 @@ def _generate_region(size, radius, noise, a, b):
     noise_x = ds.get_random_uniform(1, size, -radius, radius) * noise
     noise_y = ds.get_random_uniform(1, size, -radius, radius) * noise
     labels = _get_label((x + noise_x, y + noise_y), (0, 0), radius)
-    d = np.vstack((x, y))
-    return d, labels
+    return x, y, labels
 
 def _get_label(point, center, radius):
     v = (_dist(point, center) >= (radius * 0.5)).astype(int)
